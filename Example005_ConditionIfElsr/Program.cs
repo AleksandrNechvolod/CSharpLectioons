811513c377544f59df6174b5ffb078135ad9d75f
Aleksandr Nechvolod <alexnechvolod@gmail.com>
2023-03-27 16:00:51 +0300

    Added changes in Example002_HelloUser/ && Example005_ConditionIfElsr/

--- Example005_ConditionIfElsr/Program.cs
+++ Example005_ConditionIfElsr/Program.cs
@@ -1,16 +1,16 @@
 ﻿// Поприветствовать Машу собоенно, а другие имена обычно (Привет,  )
 
 
-Console.Write("Введите имя пользователя: ");
+// Console.Write("Введите имя пользователя: ");
 
-string username = Console.ReadLine();
-if (username.ToLower() == "маша")
-{
-  Console.WriteLine("Ура, это же Маша!");
-}
-else
-{
-  Console.Write("Привет, ");
-  Console.WriteLine(username);
-}
+// string username = Console.ReadLine();
+// if (username.ToLower() == "маша")
+// {
+//   Console.WriteLine("Ура, это же Маша!");
+// }
+// else
+// {
+//   Console.Write("Привет, ");
+//   Console.WriteLine(username);
+// }
 
